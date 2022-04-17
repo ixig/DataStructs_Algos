af4b6807d8a2213c7d82e10407e3fd3986d118d5
Wing Poon <wypoon1@gmail.com>
2022-04-17 10:24:53 -0700

    Renamed recursion to anagrams

--- anagrams.py
+++ anagrams.py
@@ -2,15 +2,15 @@
 Find all Word Anagrams
 '''
 
-def anagram(s):
+def anagrams(s):
     if not s:
         return ['']
     else:
         results = []
-        for word in anagram(s[1:]):
+        for word in anagrams(s[1:]):
             for pos in range(len(word) + 1):
                 results.append(word[:pos] + s[0] + word[pos:])
         return results
 
 tests = ['a', 'ab', 'abc']
-print([anagram(s) for s in tests])
+print([anagrams(s) for s in tests])
