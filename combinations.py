81afb2cb96c69d466ec86d029c7c7f2763d235c7
Wing Poon <wypoon1@gmail.com>
2022-04-16 22:22:35 -0700

    Implemented combinations

--- combinations.py
+++ combinations.py
@@ -1,20 +1,32 @@
-# nCr: n = # objects in set; r = # objects to choose
-def combinations(n, r):
-    output = []
-    stack = [0]
-    while stack:
-        entry = stack.pop()
-        if len(entry) == r:
-            output.append(entry)
-            continue
-        elif entry[-1]
-            entry.append(entry[-1] + 1)
-            continue
-        if entry[-1] < (n - len(entry)):
-            entry[-1] += 1
-            stack.append(entry)
+# n(C)r: n = # objects in set; r = # objects to choose
+def combinations1(n, r):
 
+    def find_solution(choices):
+        if len(choices) == r:
+            solutions.append(choices)
+            return
+
+        start_idx = choices[-1] if choices else -1
+        for choice in range(start_idx + 1, n):
+            find_solution(choices + [choice])
+        
+    solutions = []; find_solution([]); return solutions
+    
+# Select <r> objects from <selection>
+def combinations2(selection, r):
+
+    def find_solution(choices):
+        if len(choices) == r:
+            solutions.append(choices)
+            return
+
+        start_idx = selection.index(choices[-1]) if choices else -1
+        for choice in selection[start_idx + 1:]:
+            find_solution(choices + [choice])
+        return False
+        
+    solutions = []; find_solution([]); return solutions
     
 
-print(combinations(3, 2))
-# print(res)
+print(combinations1(3, 2))
+print(combinations2(['a', 'b', 'c'], 2))
